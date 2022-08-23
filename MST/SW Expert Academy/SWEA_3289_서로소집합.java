ad64219630ec1b55d4d392c506699c908e4a42e0
Seo Yeji <55950992+syg9272@users.noreply.github.com>
2022-08-24 01:26:12 +0900

    Create SWEA_3289_서로소집합.java

---
+++ MST/SW Expert Academy/SWEA_3289_서로소집합.java
@@ -0,0 +1,53 @@
+package com.ssafy.day0823;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_3289_서로소집합 {
+	static int[] p;
+	static int N, M;
+	private static void make() {
+		p = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			p[i] = i;
+		}
+	}
+	private static int find(int a) {
+		if(a == p[a]) return a;
+		return p[a] = find(p[a]);
+	}
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if(a == b) return;
+		
+		p[b] = a;
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			sb.append("#" + t + " ");
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			make();
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int type = Integer.parseInt(st.nextToken());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				if(type == 0) {
+					union(x, y);
+				}else {
+					sb.append(find(x) == find(y) ? 1 : 0);
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
