9c7725ad9aa48a840bed1c25edb363ba47576fe2
Seo Yeji <55950992+syg9272@users.noreply.github.com>
2022-11-09 22:55:13 +0900

    Create BOJ_2470_두용액.java

---
+++ Two Pointer/BOJ/BOJ_2470_두용액.java
@@ -0,0 +1,44 @@
+package study.with.minhwan;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_2470_두용액 {		// 메모리 : 32,240 kb		실행시간 : 284 ms 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		long[] arr = new long[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int left = 0;
+		int right = N - 1;
+		long ans = Integer.MAX_VALUE;
+		long a = 0;
+		long b = 0;
+		
+		Arrays.sort(arr);
+		
+		while(left < right) {
+			long mix = arr[left] + arr[right];
+			if(ans > Math.abs(mix)) {
+				ans = Math.abs(mix);
+				a = arr[left];
+				b = arr[right];
+			}
+			if(mix < 0) left++;
+			else if (mix > 0) right --;
+			else break;
+		}
+		
+		System.out.println(a + " " + b);
+	}
+}
